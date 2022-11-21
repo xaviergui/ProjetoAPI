6c544535211a632c62eaf97a6e9207ab958bb7b1
xaviergui <118702287+xaviergui@users.noreply.github.com>
2022-11-20 23:34:48 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Netflix.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IFilmeService, FilmeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
